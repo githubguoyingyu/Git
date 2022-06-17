dd95b2179a0cc2c97a38cacee581841c105e3014
gyy <7100165@qq.com>
2022-06-17 20:09:25 +0800

    first git commit

---
+++ src/main/java/com/cy/git/Demo01_Git.scala
@@ -0,0 +1,12 @@
+package com.cy.git
+
+/**
+ * @Author 未至终局
+ * @Date 2022/6/17 19:52
+ * @Version 1.0
+ */
+object Demo01_Git {
+  def main(args: Array[String]): Unit = {
+    println("hello git")
+  }
+}
